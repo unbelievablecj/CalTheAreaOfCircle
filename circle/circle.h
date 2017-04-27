6767247de0e79e62e355bba9617d3c720686b30d
ucj <unbelievablecj@outlook.com>
2017-04-27 19:19:38 +0800

    fix

--- circle/circle.h
+++ circle/circle.h
@@ -1,4 +1,5 @@
 ﻿#pragma once
+//定义圆形面积计算函数
 #ifndef calarea
 #define calarea
 double CalArea(double r);
--- circle/main.cpp
+++ circle/main.cpp
@@ -3,19 +3,19 @@
 #include"circle.h"
 using namespace std;
 int main() {
-	
+
 	double r = 0;
 	cout << "Enter the radius:";
 	cin >> r;
-	while (!cin){
-		cout <<"error,please enter again:";
-		
+	while (!cin) {
+		cout << "error,please enter again:";
+		//清空输入
 		cin.clear();
 		cin.sync();
 		cin.ignore(numeric_limits<streamsize>::max(), '\n');
 		cin >> r;
 	}
-	cout << "the area is " << CalArea(r)<<endl;
+	cout << "the area is " << CalArea(r) << endl;
 	system("pause");
 	return 0;
 }
